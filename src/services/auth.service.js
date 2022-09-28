b888e20805a8a77653cdb0c313485ce87e4ce01d
Juan Sanchez <jusanchezme@unal.edu.co>
2022-09-27 22:22:25 -0500

    Add auth jwt service

---
+++ src/services/auth.service.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8081/api/auth/";
+
+class AuthService {
+    login(username, password) {
+        return axios
+            .post(API_URL + "signin", {
+                username,
+                password
+            })
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+
+                return response.data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem("user");
+    }
+
+    register(email, password, personDTO, roleName, apartmentDTO, companyName) {
+        return axios.post(API_URL + "signup", {
+            email,
+            password,
+            personDTO,
+            roleName,
+            apartmentDTO,
+            companyName
+        });
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));;
+    }
+}
+
+export default new AuthService();
