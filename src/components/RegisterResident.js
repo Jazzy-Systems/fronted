036d3d4ecabfe4bd0aabb63dd759ffe852733c4b
Sebastian Rodriguez Ardila <sebasplay2@hotmail.com>
2022-09-27 22:10:59 -0500

    Captura de los datos
    Captura de los datos e inicio de conexión backend

--- src/components/RegisterResident.js
+++ src/components/RegisterResident.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import {useEffect,useState } from 'react';
 import '../styles/modules.css';
 const RegisterResident = (props) => {
+    const[phone, setPhone] = useState("");
+    const[apartment, setApartment] = useState("");
+
     
     return(
         <div>
             <div className="form-floating" id="input-form">
-                <input type="tel" className="form-control" id="floatingCompany" placeholder="telefono"></input>
-                <label className = "text-input" htmlFor="floatingInput">Telefono</label>
+                <input type="tel" className="form-control" id="floatingCompany" placeholder="telefono" value={phone} onChange ={(e) => setPhone(e.target.value)}></input>
+                <label className = "form-label" htmlFor="floatingInput">Telefono</label>
             </div>
             <div className="form-floating" id="input-form">
-                <input type="name" className="form-control" id="floatingPassword" placeholder="Apartamento"></input>
-                <label className = "text-input" htmlFor="floatingPassword">Apartamento</label>
+                <input type="name" className="form-control" id="floatingPassword" placeholder="Apartamento" value={apartment} onChange ={(e) => setApartment(e.target.value)}></input>
+                <label className = "form-label" htmlFor="floatingPassword">Apartamento</label>
             </div>
         </div>
     )
