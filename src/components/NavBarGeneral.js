ec6e53aa9d60bf7c5d1aa94d091a1d1e85a37c68
Sebastian Rodriguez Ardila <sebasplay2@hotmail.com>
2022-10-04 23:16:27 -0500

    Cambios barra navegacion
    Cambios en los estilos de la barra de navegación.

--- src/components/NavBarGeneral.js
+++ src/components/NavBarGeneral.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../styles/modules.css';
 const NavBarGeneral = (props) => {
     return (
-        <nav className="navbar navbar-expand navbar-dark bg-dark" id ="navBar" aria-label="Second navbar example">
+        <nav className="navbar navbar-expand navbar-dark" id ="navBar" aria-label="Second navbar example">
             <div className="container-fluid" id = "container-navBar">
                 <h1 className="d-flex align-items-center fs-1 text-black mb-0" id = "navBar-text">
                     <img className="mb-4" src={require('../images/Logo.png')} width="100" height="100" alt="JS" id = "navBar-logo"/>
