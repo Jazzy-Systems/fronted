e7a41b3047f7c4a86d301d3440dee9d9036faa7f
Sebastian Rodriguez Ardila <sebasplay2@hotmail.com>
2022-10-15 18:12:06 -0500

    Despliegue de los comunicados
    Se desarrollo la logina del despliegue de los comunicados por completo, los contenidos ya tienen estilo, puede haber modificaciones pequeñas en estilos .

--- src/components/ReleaseCard.js
+++ src/components/ReleaseCard.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import '../styles/releaseCard.css';
-import aviso from '../images/megaphone.svg'
+import aviso from '../images/aviso.svg'
+import evento from '../images/evento.svg'
+import reparacion from '../images/reparacion.svg'
 import ButtonGreen from './ButtonGreen';
 
-function ReleaseCard() {
+function ReleaseCard(props) {
+
+    const getIcon = ()=>{
+        if(props.communique.typeCommunique.typeName === "Aviso"){
+            return <div className="col-auto d-none d-lg-block">
+                <img className="mb-4" src={aviso} alt="" width="150" height="150" id = "cardIcon" />
+            </div>
+        }
+        if(props.communique.typeCommunique.typeName === "Evento"){
+            return <div className="col-auto d-none d-lg-block">
+                <img className="mb-4" src={evento} alt="" width="150" height="150" id = "cardIcon" />
+            </div> 
+        }
+        if(props.communique.typeCommunique.typeName === "Reparacion"){
+            return <div className="col-auto d-none d-lg-block">
+                <img className="mb-4" src={reparacion} alt="" width="150" height="150" id = "cardIcon" />
+            </div>
+        }
+    }
 
     return (
 
         <div className="d-md-flex flex-md-equal my-md-3 ps-md-3" id="container-cardRelease">
             <div className="row g-0 overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" id = "cardRelease">
                 <div className="col p-4 d-flex flex-column position-static" id = "p-contentCard">
-                    <strong className="d-inline-block mb-2 text-dark">Reparacion</strong>
-                    <h3 className="mb-0" >Reparación Tuberias Piso 1</h3>
-                    <div className="mb-1 text-muted">Octubre 31 2022</div>
-                    <p className="card-text" id = "p-contentCard-text">El dia 8 de septiembre del 2022, se hara una reparación por parte de la empresa
-                     PLOMERIA S.A.S a las tuberias del piso 1 por daños causados por un fuerte golpe a una tuberia, el servicio de 
-                     acueduto sera cortado entre 2:00 pm - 4: 00 pm, lamentamos los inconvenientes. </p>
+                    <strong className="d-inline-block mb-2 text-dark">{props.communique.typeCommunique.typeName}</strong>
+                    <h3 className="mb-0" >{props.communique.titleCommunique}</h3>
+                    <div className="mb-1 text-muted">{props.communique.datePublished}</div>
+                    <p className="card-text" id = "p-contentCard-text">{props.communique.description}</p>
                     <div className='more-card'>
                         <div>
                             <a href="#" className="stretched-link">Leer mas...</a>
@@ -26,9 +44,7 @@ function ReleaseCard() {
                     </div>
                     
                 </div>
-                <div className="col-auto d-none d-lg-block">
-                    <img className="mb-4" src={aviso} alt="" width="150" height="150" id = "cardIcon" />
-                </div>
+                {getIcon()}
             </div>
         </div>
     );
