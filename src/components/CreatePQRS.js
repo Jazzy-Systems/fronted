e1e315b43dac5e1eb4a3e4efdfe6e8ff0ba1f46e
Juan Sanchez <jusanchezme@unal.edu.co>
2022-11-16 19:14:01 -0500

    Style for CreatePqrs

--- src/components/CreatePQRS.js
+++ src/components/CreatePQRS.js
@@ -1,97 +1,105 @@
 import { useEffect, useState } from 'react';
-import TitleCard from './TitleCard';
 import ButtonGreen from './ButtonGreen';
-import '../styles/createCommunique.css';
+import '../styles/basic.css';
 import pqrsService from '../services/pqrs-service';
 import authHeader from '../services/auth-header';
+import AlertNavigate from './AlertNavigate';
 
 const CreatePqrs = (props) => {
-    const [form, setForm] = useState({ type: "" });
-    const [resMessage, setResMessage] = useState(null);
-    const [typeRequests, setTypeRequests] = useState(null);
-    const API_URL = process.env.REACT_APP_API_URL;
+  const [form, setForm] = useState({ type: "" });
+  const [resMessage, setResMessage] = useState(null);
+  const [typeRequests, setTypeRequests] = useState(null);
+  const API_URL = process.env.REACT_APP_API_URL;
+  const [navigateTo, setNavigateTo] = useState("/profile");
+  const [success, setSuccess] = useState(null);
 
-    const handleOnChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
-        console.log(form)
+  const handleOnChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+    console.log(form)
+  }
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      headers: authHeader()
     }
-
-    useEffect(() => {
-        const requestOptions = {
-            method: 'GET',
-            headers: authHeader()
-        }
-        const dataFetch = async () => {
-            const data = await (
-                await fetch(
-                    API_URL + "/api/v1/typerequest/", requestOptions
-                )
-            ).json();
-            setTypeRequests(data);
-        };
-        dataFetch();
-    }, [API_URL]);
-
-    const newPQRS = (e) => {
-        e.preventDefault();
-        console.log(form)
-        const responseRequest = null;
-        const statusRequest = false;
-        const typeRequest = {
-            typeRequestName: form.typeRequestName
-        }
-        pqrsService.save(form.titleRequest,
-            form.descriptionRequest,
-            responseRequest,
-            statusRequest,
-            typeRequest
-        ).then(
-            () => {
-                alert("Se ha enviado la PQRS, existosamente.")
-            },
-            error => {
-                setResMessage(
-                    (error.response &&
-                        error.response.data &&
-                        error.response.data.message) ||
-                    error.message ||
-                    error.toString());
-            }
-        );
-    }
-
-    if (typeRequests) {
-        return (
-            <div class='container-md justify-center'>
-                <TitleCard text="Realizar una PQRS" />
-                <label htmlFor="selector" className="form-label">Tipo</label>
-                <form className='form-Communique' onSubmit={newPQRS}>
-                    <select required name="typeRequestName" className="form-select" id="type-selector" value={form.typeRequestName} onChange={handleOnChange}>
-                        <option></option>
-                        {typeRequests.map((typeRequest) => (
-                            <option value={typeRequest.typeRequestName}>
-                                {typeRequest.typeRequestName}
-                            </option>
-                        ))}
-                    </select>
-                    <div className="form-floating" id="input-form">
-                        <input type="text" name="titleRequest" className="form-control" id="floatingName" placeholder="name" value={form.titleRequest} onChange={handleOnChange} required></input>
-                        <label className="form-label" htmlFor="floatingInput">Titulo</label>
-                    </div>
-                    <div className="form-floating" id="input-formDescripcion">
-                        <textarea type="text" name="descriptionRequest" className="form-control" id="floatingDescripcion" placeholder="descripcion" value={form.descriptionRequest} onChange={handleOnChange} required></textarea>
-                        <label className="form-label" htmlFor="floatingInput">Descripcion</label>
-                    </div>
-
-                    <ButtonGreen id="submit-button" text="Crear" type="Submit" />
-                </form>
-                <div>
-                    <span>{resMessage}</span>
-                </div>
-            </div>
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          API_URL + "/api/v1/typerequest/", requestOptions
         )
+      ).json();
+      setTypeRequests(data);
+    };
+    dataFetch();
+  }, [API_URL]);
+
+  const newPQRS = (e) => {
+    e.preventDefault();
+    const responseRequest = null;
+    const statusRequest = false;
+    const typeRequest = {
+      typeRequestName: form.typeRequestName
     }
-    else return (<div><h2>Cargando</h2></div>)
+    pqrsService.save(form.titleRequest,
+      form.descriptionRequest,
+      responseRequest,
+      statusRequest,
+      typeRequest
+    ).then(
+      () => {
+        setSuccess(true);
+        setResMessage("Se ha enviado la PQRS, existosamente.")
+        setNavigateTo("/profile")
+      },
+      error => {
+        setResMessage(
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString());
+        setSuccess(false);
+        setNavigateTo("/profile/createpqrs")
+      }
+    );
+  }
+
+  if (typeRequests) {
+    return (
+      <div className='card container-sm d-flex justify-content-center align-items-center col-auto' id="card">
+        <form className='container d-flex flex-column' id="form" onSubmit={newPQRS}>
+          <h3 className='mx-auto flex align-center fw-bold' id="title">Formulario PQRS</h3>
+          <label htmlFor="type-selector" className="form-label">Tipo</label>
+          <select required name="typeRequestName" className="form-select" id="type-selector"
+            value={form.typeRequestName} onChange={handleOnChange}>
+            <option>Seleccione el tipo de PQRS</option>
+            {typeRequests.map((typeRequest) => (
+              <option value={typeRequest.typeRequestName}>
+                {typeRequest.typeRequestName}
+              </option>
+            ))}
+          </select>
+          <div className="form-floating" id="input-form">
+            <input type="text" name="titleRequest" className="form-control" id="floatingInput" placeholder="name" value={form.titleRequest} onChange={handleOnChange} required></input>
+            <label className="form-label" htmlFor="floatingInput">Titulo</label>
+          </div>
+          <div className="mb-4">
+            <div className="form-floating" id="input-form">
+              <textarea id="textarea" type="textarea" name="descriptionRequest" className="form-control" placeholder="descripcion" value={form.descriptionRequest} onChange={handleOnChange} required></textarea>
+              <label className="form-label" htmlFor="floatingInput">Descripcion</label>
+            </div>
+          </div>
+
+          <div className="mx-auto my-auto mt-4">
+            <ButtonGreen id="submit-button" text="Realizar PQRS" type="Submit" />
+          </div>
+        </form>
+        {success != null && (<AlertNavigate success={success} resMessage={resMessage} navigateTo={navigateTo} />)}
+      </div>
+    )
+  }
+  else return (<div><h2>Cargando</h2></div>)
 }
 
 export default CreatePqrs;
